603473bd2e56fcc149a38ea4c8986c2f56aa8d15
Enes Goksu <enes-goksu.fb@hotmail.com>
2023-03-24 22:14:27 +0300

    son hali uygulamalar

--- Program.cs
+++ Program.cs
@@ -7,73 +7,73 @@
             // Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n). Sonrasında kullanıcadan n adet pozitif sayı girmesini isteyin. Kullanıcının girmiş olduğu sayılardan çift olanları console'a yazdırın.
 
 
-            Console.Write("Pozitif bir sayı giriniz: ");
-            int pozitifSayi = int.Parse(Console.ReadLine());
-            Console.WriteLine(+pozitifSayi +" "+"adet sayı giriniz.");
-            int[] sayilar = new int [pozitifSayi];
+            // Console.Write("Pozitif bir sayı giriniz: ");
+            // int pozitifSayi = int.Parse(Console.ReadLine());
+            // Console.WriteLine(+pozitifSayi +" "+"adet sayı giriniz.");
+            // int[] sayilar = new int [pozitifSayi];
             
             
 
-            for (int i = 0; i < pozitifSayi; i++)
-            {
-                Console.Write("Sayı giriniz: ");
-                int sayi = int.Parse(Console.ReadLine());
-                sayilar[i] = sayi;           
-            }
+            // for (int i = 0; i < pozitifSayi; i++)
+            // {
+            //     Console.Write("Sayı giriniz: ");
+            //     int sayi = int.Parse(Console.ReadLine());
+            //     sayilar[i] = sayi;           
+            // }
 
-            for (int i = 0; i < pozitifSayi; i++)
-            {
-                if (sayilar[i] % 2 == 0)
-                {
-                    Console.WriteLine("Girmiş olduğunuz çift sayı: " + sayilar[i]);
-                }
-            }
+            // for (int i = 0; i < pozitifSayi; i++)
+            // {
+            //     if (sayilar[i] % 2 == 0)
+            //     {
+            //         Console.WriteLine("Girmiş olduğunuz çift sayı: " + sayilar[i]);
+            //     }
+            // }
 
             // Bir konsol uygulamasında kullanıcıdan pozitif iki sayı girmesini isteyin (n,m). Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. Kullanıcının girmiş olduğu sayılardan m'e eşit ya da tam bölünenleri console'a yazdırın.
 
-            Console.Write("1.sayıyı giriniz: ");
-            int sayi1 = int.Parse(Console.ReadLine());
-            Console.Write("2.sayıyı giriniz: ");
-            int sayi2 = int.Parse(Console.ReadLine());
-            Console.WriteLine(+ sayi1+" "+"adet sayı giriniz.");
-            int[] sayilar = new int [sayi1];
+            // Console.Write("1.sayıyı giriniz: ");
+            // int sayi1 = int.Parse(Console.ReadLine());
+            // Console.Write("2.sayıyı giriniz: ");
+            // int sayi2 = int.Parse(Console.ReadLine());
+            // Console.WriteLine(+ sayi1+" "+"adet sayı giriniz.");
+            // int[] sayilar = new int [sayi1];
 
-            for (int i = 0; i < sayi1; i++)
-            {
-                Console.Write("{0}.sayıyı giriniz: ", i+1);
-                int sayi = int.Parse(Console.ReadLine());
-                sayilar[i] = sayi;
-            }
+            // for (int i = 0; i < sayi1; i++)
+            // {
+            //     Console.Write("{0}.sayıyı giriniz: ", i+1);
+            //     int sayi = int.Parse(Console.ReadLine());
+            //     sayilar[i] = sayi;
+            // }
             
-            for (int i = 0; i < sayi1; i++)
-            {
-                if ((sayilar[i] == sayi2) || (sayilar[i] % sayi2 == 0))
-                {
-                    Console.WriteLine("Koşulunu sağlayan sayı: "+ sayilar[i]);
-                }
-            }
+            // for (int i = 0; i < sayi1; i++)
+            // {
+            //     if ((sayilar[i] == sayi2) || (sayilar[i] % sayi2 == 0))
+            //     {
+            //         Console.WriteLine("Koşulunu sağlayan sayı: "+ sayilar[i]);
+            //     }
+            // }
 
             // Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n). Sonrasında kullanıcıdan n adet kelime girmesini isteyin. Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın.
 
-            Console.Write("Bir sayı giriniz: ");
-            int sayi = int.Parse(Console.ReadLine());
-            Console.WriteLine("Bir sonraki aşamada"+" "+sayi+" "+"adet kelime girmeniz istenecektir.");
-            string[] kelimeler = new string [sayi];
+            // Console.Write("Bir sayı giriniz: ");
+            // int sayi = int.Parse(Console.ReadLine());
+            // Console.WriteLine("Bir sonraki aşamada"+" "+sayi+" "+"adet kelime girmeniz istenecektir.");
+            // string[] kelimeler = new string [sayi];
 
-            for (int i = 0; i < sayi; i++)
-            {
-                Console.WriteLine("{0}. girmek istediğiniz kelimeyi girin: ", i + 1);
-                string kelime = Console.ReadLine();
-                kelimeler[i] = kelime;
-            }
+            // for (int i = 0; i < sayi; i++)
+            // {
+            //     Console.WriteLine("{0}. girmek istediğiniz kelimeyi girin: ", i + 1);
+            //     string kelime = Console.ReadLine();
+            //     kelimeler[i] = kelime;
+            // }
 
-                Console.WriteLine("***********");
+            //     Console.WriteLine("***********");
 
-                Array.Reverse(kelimeler);
-                foreach (var kelime1 in kelimeler)
-                {
-                    Console.WriteLine(kelime1);
-                }
+            //     Array.Reverse(kelimeler);
+            //     foreach (var kelime1 in kelimeler)
+            //     {
+            //         Console.WriteLine(kelime1);
+            //     }
 
 
             // Bir konsol uygulamasında kullanıcıdan bir cümle yazmasını isteyin. Cümledeki toplam kelime ve harf sayısını console'a yazdırın.
@@ -87,6 +87,7 @@
             {
                 Console.WriteLine(kelime);
             }
+
 
             Console.WriteLine("Toplam harf: "+cümle.Length);
 
